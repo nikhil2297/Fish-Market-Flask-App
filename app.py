b503be71e38598d71639264e819c49f823250d6f
Nikhil Lohar <nikhillohar2297@gmail.com>
2024-03-18 20:24:49 -0400

    Changes in folder strcutre

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ def predict() :
     return jsonify({"predicted_species": species_names[int(prediction)]})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
